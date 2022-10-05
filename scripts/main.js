15bc31aad659ee89b85e5e292c9785e53bab4263
Stanno500 <stanvanbentum@hotmail.com>
2022-10-05 10:31:37 +0200

    Scrolling almost works

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 function test() {
-    document.getElementsByClassName('left')[0].innerHTML += "<p>test</p>"
+    //document.getElementsByClassName('left')[0].innerHTML += "<p>test</p>"
     console.log("scrolled!")
 }
 
-window.addEventListener("scroll", test)
 window.onload = function () {
-    
 
+    document.getElementsByClassName('right')[0].addEventListener("scroll", test)
 }
---
+++ scripts/scroll.js
@@ -0,0 +1,40 @@
+const orb = document.querySelector('.left'),
+    ease = 0.2,
+    start_position = 0;
+let animation_running = false,
+    total_offset = 0,
+    scroll_request = 0;
+
+function animate_scroll() {
+    scroll_request++;
+
+    if (!animation_running) {
+        animation_running = true;
+
+        animation_loop();
+    }
+}
+
+function animation_loop() {
+    console.log('test')
+    const current_offset = window.pageYOffset;
+
+    let difference = current_offset - (total_offset*0.8);
+    difference *= ease;
+
+    if (Math.abs(difference) < 0.05) {
+        scroll_request = 0;
+        total_offset = current_offset;
+        animation_running = false;
+
+        return;
+    }
+
+    orb.style.top = `${start_position - total_offset}px`;
+
+    total_offset += difference;
+
+    requestAnimationFrame(animation_loop);
+}
+
+document.addEventListener('scroll', animate_scroll);
